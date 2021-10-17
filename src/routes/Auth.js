477c3871eb93d2b82d88ee3bf1d95cb36dadb69d
Pyunghyuk Yoo <yoophi@gmail.com>
2021-10-17 19:29:02 +0900

    :fire: REMOVE: unused console.log

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -22,16 +22,11 @@ const Auth = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
-      let data;
       if (newAccount) {
-        data = await authService.createUserWithEmailAndPassword(
-          email,
-          password
-        );
+        await authService.createUserWithEmailAndPassword(email, password);
       } else {
-        data = await authService.signInWithEmailAndPassword(email, password);
+        await authService.signInWithEmailAndPassword(email, password);
       }
-      console.log({ data });
     } catch (error) {
       setError(error.message);
     }
@@ -48,8 +43,7 @@ const Auth = () => {
     } else if (name === "github") {
       provider = new firebaseInstance.auth.GithubAuthProvider();
     }
-    const data = await authService.signInWithPopup(provider);
-    console.log({ data });
+    await authService.signInWithPopup(provider);
   };
 
   return (
