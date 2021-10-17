37a8672060512eb1c74445cb8a01026926bb3ecb
Pyunghyuk Yoo <yoophi@gmail.com>
2021-10-17 18:26:31 +0900

    :sparkles: NEW: nav, profile page, handle logout

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,24 @@
+import Navigation from "components/Navigation";
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
-          <Route exact path="/">
-            <Home />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
         ) : (
           <Route exact path="/">
             <Auth />
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,19 @@
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <div>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </div>
+  );
 };
 
 export default Profile;
