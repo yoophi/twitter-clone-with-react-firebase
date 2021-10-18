801e00fb1510f2483b2293a2007bec2f5e182b25
Pyunghyuk Yoo <yoophi@gmail.com>
2021-10-19 01:10:09 +0900

    :sparkles: NEW: show user tweets in Profile page

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,18 +1,48 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Tweet from "components/Tweet";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const [tweets, setTweets] = useState([]);
+
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
 
+  const getMyTweets = async () => {
+    const tweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt", "asc")
+      .get();
+    return tweets.docs.map((document) => ({
+      ...document.data(),
+      id: document.id,
+    }));
+  };
+
+  useEffect(() => {
+    getMyTweets().then((tweets) => setTweets(tweets));
+  }, []);
+
   return (
-    <div>
-      <button onClick={onLogOutClick}>Log Out</button>
-    </div>
+    <>
+      <div>
+        <button onClick={onLogOutClick}>Log Out</button>
+      </div>
+      <div>
+        {tweets.map((tweet) => (
+          <Tweet
+            key={tweet.id}
+            tweet={tweet}
+            isOwner={tweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
