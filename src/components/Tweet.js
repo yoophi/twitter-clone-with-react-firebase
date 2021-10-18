c75123e4754cdc01932a1e1bc9620c05e42a0eec
Pyunghyuk Yoo <yoophi@gmail.com>
2021-10-18 22:12:59 +0900

    :sparkles: NEW: handle delete tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,20 @@
+import { dbService } from "fbase";
 import React from "react";
 
 const Tweet = ({ tweet, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this tweet?");
+    if (ok) {
+      await dbService.doc(`tweets/${tweet.id}`).delete();
+    }
+  };
+
   return (
     <div>
       <h4>{tweet.text}</h4>
       {isOwner && (
         <div>
-          <button>Delete Tweet</button>
+          <button onClick={onDeleteClick}>Delete Tweet</button>
           <button>Edit Tweet</button>
         </div>
       )}
